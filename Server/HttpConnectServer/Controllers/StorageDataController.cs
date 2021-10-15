1f473f1f5f04cd9f15173ab8b0baba3dc66fac2a
ryan4gitee <rliang.lai@outlook.com>
2021-10-15 22:28:38 +0800

    init client and server files.

---
+++ Server/HttpConnectServer/Controllers/StorageDataController.cs
@@ -0,0 +1,47 @@
+﻿using HttpConnectServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpConnectServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StorageDataController : ControllerBase
+    {
+        private readonly ILogger<StorageDataController> _logger;
+
+        public StorageDataController(ILogger<StorageDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetData(string name)
+        {
+            var result = $"Hello, {name}";
+
+            return result;
+        }
+
+        [HttpPost]
+        public string PostData([FromForm]GameData data)
+        {
+            var result = "";
+
+            if (data.Name == "admin" && data.Password == "123")
+            {
+                result = $"Welcome {data.Name}, login successfuly!";
+            }
+            else
+            {
+                result = $"Opppps! {data.Name} might check your password!";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Server/HttpConnectServer/Models/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpConnectServer.Models
+{
+    public class GameData
+    {
+        public string Name { set; get; }
+        public string Password { set; get; }
+    }
+}
